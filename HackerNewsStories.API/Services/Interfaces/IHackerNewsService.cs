1bef3e38c181d3578d5f1bec19a0603d129a5918
kfros <kfrosyniak@interia.pl>
2024-12-08 16:17:50 +0100

    Git cache clearing

--- HackerNewsStories.API/Services/Interfaces/IHackerNewsService.cs
+++
@@ -1,9 +0,0 @@
-﻿using HackerNewsStories.API.Models;
-
-namespace HackerNewsStories.API.Services.Interfaces
-{
-    public interface IHackerNewsService
-    {
-        Task<IEnumerable<Story>> GetBestStoriesAsync(int count);
-    }
-}
